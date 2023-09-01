19dd4fda076f9825effff8ad7a94e07fc6f45734
Oleh Pshyk <pshykol@gmail.com>
2023-09-01 16:04:13 +0300

    Changes after comments

--- PotionInventory/PotionInventory/Logger.cs
+++ PotionInventory/PotionInventory/Logger.cs
@@ -6,4 +6,13 @@ public class Logger
     {
         Console.WriteLine(message);
     }
+
+    public void LogPotions(List<Potion> potions)
+    {
+        foreach (Potion potion in potions)
+        {
+            Console.WriteLine(
+                $"Name: {potion.PotionName} | Type: {potion.PotionType} | Action: {potion.PotionAction} | Recovery: {potion.PotionRecovery} | Damage: {potion.PotionDamage}\n");
+        }
+    }
 }
--- PotionInventory/PotionInventory/Program.cs
+++ PotionInventory/PotionInventory/Program.cs
@@ -10,39 +10,7 @@ public class Program
     {
         if (JsonFileReader.CheckIfFileExists())
         {
-            while (true)
-            {
-                _logger.Log("Type 'attack' or 'restoration' to choose potion type that you want or 'q' to exit:\n");
-                string input = InputReader.ReadInput();
-                if (input.ToLower() == "attack")
-                {
-                    List<Potion> filteredPotions = _jsonFilter.ReadPotions(input);
-                    LogPotions(filteredPotions);
-                }
-                else if (input.ToLower() == "restoration")
-                {
-                    List<Potion> filteredPotions = _jsonFilter.ReadPotions(input);
-                    LogPotions(filteredPotions);
-                }
-                else if (input.ToLower() == "q")
-                {
-                    _logger.Log("See you soon!");
-                    break;
-                }
-                else
-                {
-                    _logger.Log("JSON file does not exist!");
-                }
-            }
-
-            static void LogPotions(List<Potion> potions)
-            {
-                foreach (Potion potion in potions)
-                {
-                    _logger.Log(
-                        $"Name: {potion.PotionName} | Type: {potion.PotionType} | Action: {potion.PotionAction} | Recovery: {potion.PotionRecovery} | Damage: {potion.PotionDamage}\n");
-                }
-            }
+                InputValidation.ValidInput();
         }
     }
 }
